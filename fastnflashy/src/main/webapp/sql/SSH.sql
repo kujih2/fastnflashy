69887f53564c456039e6374f0da90ea32f0da804
user <user@DESKTOP-COJ9858>
2023-12-08 17:44:51 +0900

    테이블 작성 수정

--- fastnflashy/src/main/webapp/sql/SSH.sql
+++ fastnflashy/src/main/webapp/sql/SSH.sql
@@ -9,6 +9,14 @@ create table magazin_board (
  mg_filename varchar2(256) not null,
  mg_video varchar2(900),
  mg_ip varchar2(40) not null,
- mem_num number not null,
+ mem_num number not null
 );
-create sequance maga
+create sequance magazin_seq;
+
+create table magazin_reply (
+ mg_re_num number not null,
+ mg_isdeleted number(1) not null,
+ mg_content varchar2(900) not null,
+ mg_reg_date date default sysdate not null,
+ mg_modify_date date
+);
