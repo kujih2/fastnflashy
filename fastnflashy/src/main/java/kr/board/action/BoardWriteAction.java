c67bf04889f1bc5c33ac12002d695ead55627cbb
tjsco <tjsco@DESKTOP-OF990J6>
2023-12-13 19:46:07 +0900

    Board 보충

---
+++ fastnflashy/src/main/java/kr/board/action/BoardWriteAction.java
@@ -0,0 +1,41 @@
+package kr.board.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.oreilly.servlet.MultipartRequest;
+
+import kr.board.dao.BoardDAO;
+import kr.board.vo.BoardVO;
+import kr.controller.Action;
+import kr.util.FileUtil;
+
+public class BoardWriteAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		HttpSession session = request.getSession();
+		Integer user_num = (Integer)session.getAttribute("user_num");
+		
+		if(user_num==null) {//로그인이 되지 않은 경우
+			return "redirect:/member/loginForm.do";
+		}
+		//로그인 된 경우
+		MultipartRequest multi = FileUtil.createFile(request);
+		BoardVO board = new BoardVO();
+		board.setTitle(multi.getParameter("title"));
+		board.setBoard_category(Integer.parseInt(multi.getParameter("category")));
+		board.setContent(multi.getParameter("content"));
+		board.setIp(request.getRemoteAddr());
+		board.setFilename(multi.getFilesystemName("filename"));
+		board.setMem_num(user_num);
+		
+		BoardDAO dao = BoardDAO.getInstance();
+		dao.insertBoard(board);
+		
+		return "/WEB-INF/views/board/boardWrite.jsp";
+	}
+
+}
